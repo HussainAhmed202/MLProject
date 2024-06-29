a7a3292fecff1b84d1bd842f5a2a9d58600f611c
Syeda Aiman Naveed <pc9763.aiman@gmail.com>
2024-06-29 22:51:09 +0500

    pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -4,9 +4,11 @@ from dataclasses import dataclass
 import numpy as np 
 import pandas as pd
 from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
+from sklearn.impute import KNNImputer,SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder,StandardScaler
+from sklearn.preprocessing import StandardScaler,MinMaxScaler,FunctionTransformer,RobustScaler
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.feature_selection import SelectKBest
 from src.utils import save_object
 from src.exception import CustomException
 from src.logger import logging
@@ -22,31 +24,85 @@ class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
 
-    def get_data_transformer_object(self):
+    def get_data_transformer_object(self,train_path):
         '''
-        This function si responsible for data trnasformation
+        This function is responsible for data trnasformation
         
         '''
         try:
-            numerical_columns = ["age", "sex" , "cp", "trestbps" , "chol", "fbs", "restecg", "thalach","exang","oldpeak","slope","ca","thal"]
-            num_pipeline= Pipeline(
-                steps=[
-                ("imputer",SimpleImputer(strategy="median")),
-                ("scaler",StandardScaler())
+            target_column_name="num"
+            train_df=pd.read_csv(train_path)
+            X_train=train_df.drop(columns=[target_column_name],axis=1)
 
+
+            numeric_feat = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
+            categorical_feat = ['sex', 'restecg', 'thal', 'slope', 'cp', 'exang', 'ca','fbs']
+
+            # Categorical Features PipeLines
+            categorical__feat_pipeline = Pipeline(
+                steps = [
+                ("knn_imputer", KNNImputer(n_neighbors=1))
+                ]
+            )
+            logging.info(f"Categorical columns: {categorical_feat}")
+
+
+            # Numeric Features PipeLines
+            numeric_feat_pipeline = Pipeline(
+                steps = [
+                ("scaler", RobustScaler())
                 ]
             )
 
-            logging.info(f"Numerical columns: {numerical_columns}")
-
-            preprocessor=ColumnTransformer(
-                [
-                ("num_pipeline",num_pipeline,numerical_columns)
-
-                ]
+            logging.info(f"Numerical columns: {numeric_feat}")
 
 
+            preprocessor1 = ColumnTransformer(
+                transformers = [
+                    ("knn_imputation", categorical__feat_pipeline, ['ca','thal']),
+                    ("numeric_pipeline", numeric_feat_pipeline, numeric_feat)
+                ],verbose_feature_names_out = False,
+                remainder = "passthrough"
             )
+            preprocessor1
+
+            print("Index of categorical features in X_train")
+            L = []
+            for i in range(len(X_train.columns)):
+                if X_train.columns[i] in categorical_feat:
+                    L.append(i)
+            print(L)
+
+            def custom_mutual_info_classif(X,y):
+                return mutual_info_classif(X, y, discrete_features=L)
+
+            preprocessor = Pipeline (
+                steps = [
+                    ("feature_eng_steps", preprocessor1),
+                    ("feature_selection_steps", SelectKBest(score_func=custom_mutual_info_classif,k=10))
+                ]
+            ).set_output(transform='pandas')
+
+            # ///////////////////////////////////////////////////////////////
+            # numerical_columns = ["age", "sex" , "cp", "trestbps" , "chol", "fbs", "restecg", "thalach","exang","oldpeak","slope","ca","thal"]
+            # num_pipeline= Pipeline(
+            #     steps=[
+            #     ("imputer",SimpleImputer(strategy="median")),
+            #     ("scaler",StandardScaler())
+
+            #     ]
+            # )
+
+            # logging.info(f"Numerical columns: {numerical_columns}")
+
+            # preprocessor=ColumnTransformer(
+            #     [
+            #     ("num_pipeline",num_pipeline,numerical_columns)
+
+            #     ]
+
+
+            # )
 
             return preprocessor
         
@@ -63,11 +119,12 @@ class DataTransformation:
 
             logging.info("Obtaining preprocessing object")
 
-            preprocessing_obj=self.get_data_transformer_object()
+            preprocessing_obj=self.get_data_transformer_object(train_path)
 
             target_column_name="num"
-            numerical_columns = ["age", "sex" , "cp", "trestbps" , "chol", "fbs", "restecg", "thalach","exang","oldpeak","slope","ca","thal"]
-
+            numeric_feat = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
+            categorical_feat = ['sex', 'restecg', 'thal', 'slope', 'cp', 'exang', 'ca','fbs']
+            
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
