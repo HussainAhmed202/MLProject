956f60192bc13cabbf6e9749b227f1618c5e2a35
Syeda Aiman Naveed <pc9763.aiman@gmail.com>
2024-06-30 14:31:34 +0500

    model_training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,7 +12,8 @@ from src.components.data_transformation import DataTransformation
 from src.exception import CustomException
 from src.logger import logging
 
-
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     """Stores the path for train and test data"""
@@ -96,5 +97,10 @@ if __name__ == "__main__":
         train_data, test_data = obj.initiate_data_ingestion()
         data_transformation = DataTransformation()
         data_transformation.initiate_data_transformation(train_data, test_data)
+
+        train_arr, test_arr,_ =data_transformation.initiate_data_transformation(train_data, test_data)
+        modeltrainer= ModelTrainer()
+        print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+
     except Exception as e:
         raise CustomException(e, sys)
