8e4139c54c03fe678141a61d9af6df7c3425bf72
hussain <h2002ahmed@gmail.com>
2024-07-05 19:29:32 +0500

    rm custom scorer

--- src/utils.py
+++ src/utils.py
@@ -5,21 +5,10 @@ import sys
 import dill
 import numpy as np
 import pandas as pd
-from sklearn.metrics import confusion_matrix, make_scorer
 from sklearn.model_selection import GridSearchCV, cross_val_score
 
 from src.exception import CustomException
 from src.logger import logging
-
-
-def performance_metric_score(y_true, y_pred):
-    """Calculate the average specificity and sensitvity
-    from confusion matrix."""
-
-    tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
-    specificty = tn / (tn + fp)
-    sensitivity = tp / (tn + fn)  # recall
-    return (specificty + sensitivity) / 2
 
 
 def save_object(file_path, obj):
@@ -39,9 +28,6 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        # Create a custom scorer for specificity
-        model_scorer = make_scorer(performance_metric_score)
-
         for i in range(len(list(models))):
             model = list(models.values())[i]
             para = param[list(models.keys())[i]]
@@ -49,27 +35,22 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             print(f"Applying grid search for {type(model).__name__}")
             logging.info(f"Applying grid search for {type(model).__name__}")
 
-            gs = GridSearchCV(model, para, cv=5, scoring=model_scorer)
+            gs = GridSearchCV(model, para, cv=5, scoring="recall")
             gs.fit(X_train, y_train)
 
             model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
-            # # train_model_score = accuracy_score(y_train, y_train_pred)
-            # train_model_score = cross_val_score(
-            #     model, X_train, y_train, scoring="accuracy", cv=5
-            # ).mean()
             train_model_score = cross_val_score(
-                model, X_train, y_train, scoring=model_scorer, cv=5
+                model, X_train, y_train, scoring="recall", cv=5
             ).mean()
 
             print(
                 f"{type(model).__name__} got the overall score on train data {train_model_score}"
             )
 
-            # # test_model_score = accuracy_score(y_test, y_test_pred)
             test_model_score = cross_val_score(
-                model, X_test, y_test, scoring=model_scorer, cv=5
+                model, X_test, y_test, scoring="recall", cv=5
             ).mean()
 
             print(
